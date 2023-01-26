e23c8c86a2e72faa8a0b57398a8ea3f091afc2a5
OliviaA22 <olivia_adaugo@yahoo.com>
2023-01-26 17:17:24 +0100

    First Commit

---
+++ app.py
@@ -0,0 +1,68 @@
+# from dotenv import load_dotenv
+
+# load_dotenv()
+from flask import Flask, render_template
+
+import json
+
+with open("Data.json") as f:
+    data = json.load(f)
+
+def set_location(data):
+    left = []
+    right =[]
+    for patient in data:
+        if patient['Location'] == 'Left':
+            left.append(patient)
+        else:
+            right.append(patient)
+    return data
+data= set_location(data)
+
+def set_medication_status(data):
+    for patient in data:
+        if patient['Given'] == 0:
+            patient['Status'] = 'red' 
+        elif patient['Prescribed'] == patient['Given']:
+            patient['Status'] = 'green'
+        else:
+            patient['Status'] = 'orange'
+    return data
+
+data = set_medication_status(data)
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hospital():
+    wards = list(set([patient["Ward"] for patient in data]))
+    return render_template(
+        "wards.html",
+        data = wards,
+    )
+
+
+
+@app.route('/<string:ward>')
+def wards(ward):
+    filtered_data = [patient for patient in data if patient["Ward"] == ward]
+    sorted_data = sorted(filtered_data, key=lambda patient: int(patient["RoomNo"]))
+    return render_template(
+        "patients.html",
+        data = sorted_data,
+    )
+
+@app.route('/details/<patient_id>')
+def patient_details(patient_id):
+    with open('data.json') as f:
+        data = json.load(f)
+    patient_data = next(patient for patient in data if patient["PatientID"] == patient_id)
+    return render_template("details.html", patient=patient_data,
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
---
+++ static/scripts.js
@@ -0,0 +1,8 @@
+    
+  var changeColorButton = document.getElementById("change-color-button");
+  changeColorButton.addEventListener("click", function() {
+    var rooms = document.getElementsByClassName("rooms");
+    for (var i = 0; i < rooms.length; i++) {
+        rooms[i].style.backgroundColor = "red";
+      }
+    }); 
