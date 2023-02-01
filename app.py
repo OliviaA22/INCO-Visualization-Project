710fd2229b3142768cd3a88b54f4e77a05e17e74
OliviaA22 <olivia_adaugo@yahoo.com>
2023-02-01 15:25:04 +0100

    button functional and details accessible

--- app.py
+++ app.py
@@ -8,29 +8,33 @@ import json
 with open("Data.json") as f:
     data = json.load(f)
 
+
 def set_location(data):
     left = []
-    right =[]
+    right = []
     for patient in data:
         if patient['Location'] == 'Left':
             left.append(patient)
         else:
             right.append(patient)
     return data
-data= set_location(data)
+
+
+data = set_location(data)
+
 
 def set_medication_status(data):
     for patient in data:
         if patient['Given'] == 0:
-            patient['Status'] = 'red' 
+            patient['Status'] = 'red'
         elif patient['Prescribed'] == patient['Given']:
             patient['Status'] = 'green'
         else:
             patient['Status'] = 'orange'
     return data
 
-data = set_medication_status(data)
 
+data = set_medication_status(data)
 
 
 app = Flask(__name__)
@@ -41,28 +45,27 @@ def hospital():
     wards = list(set([patient["Ward"] for patient in data]))
     return render_template(
         "wards.html",
-        data = wards,
+        data=wards,
     )
-
 
 
 @app.route('/<string:ward>')
 def wards(ward):
     filtered_data = [patient for patient in data if patient["Ward"] == ward]
-    sorted_data = sorted(filtered_data, key=lambda patient: int(patient["RoomNo"]))
+    sorted_data = sorted(
+        filtered_data, key=lambda patient: int(patient["RoomNo"]))
     return render_template(
         "patients.html",
-        data = sorted_data,
+        data=sorted_data,
     )
 
-@app.route('/details/<patient_id>')
-def patient_details(patient_id):
-    with open('data.json') as f:
-        data = json.load(f)
-    patient_data = next(patient for patient in data if patient["PatientID"] == patient_id)
-    return render_template("details.html", patient=patient_data,
-    )
+
+@app.route('/<string:ward>/details/<int:Room>')
+def details(ward, Room):
+    patient_data = next(patient for patient in data if patient["Ward"] == ward and patient["RoomNo"] == Room)
+    return render_template("details.html", data=patient_data)
+
 
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
